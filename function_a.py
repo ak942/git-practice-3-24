85144ba391a48766411fab2dd8d6cdddf000a7fd
Agnes Kim <agneskim4@gmail.com>
2023-03-24 15:29:14 -0400

    chnaged functiona to return the highest value

--- function_a.py
+++ function_a.py
@@ -1,7 +1,19 @@
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
-    pass
+    common_numbers = {}
+    for nums in number_list:
+        if nums in common_numbers:
+            common_numbers[nums] +=1
+        else:
+            common_numbers[nums] = 1
+    highest_number = 0
+    highest_count = 0
+    for key, value in common_numbers.items():
+        if value > highest_count:
+            highest_count = value
+            highest_number = key
+    return highest_number
 
 
 if __name__ == "__main__":
