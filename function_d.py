10887a77e6546fbcc7aa5b203ce4c9f631e3f2a3
Sydney Alberson <sydney.alberson@gmail.com>
2023-03-24 14:01:28 -0400

    "return Hello"

--- function_d.py
+++ function_d.py
@@ -2,10 +2,8 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
+    return "hello"
 
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
-
-    return "hello"
